6382dfe7b187817c3bc8edaf08494a3528e32b9a
Adnan khan <IGNITION420@GMAIL.COM>
2024-09-16 16:56:24 +0500

    backround

--- Environment/src/App.jsx
+++ Environment/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       <OrbitControls makeDefault />
 
       <directionalLight position={[1, 2, 3]} intensity={1.5} />
-      <ambientLight intensity={0.5} />
+      <ambientLight intensity={1} />
 
       <mesh position-x={-2}>
         <sphereGeometry />
--- Environment/src/main.jsx
+++ Environment/src/main.jsx
@@ -4,16 +4,20 @@ import App from "./App.jsx";
 import "./index.css";
 import { Canvas } from "@react-three/fiber";
 
+const created = ({ gl }) => {
+  gl.setClearColor("ivory", 1);
+};
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Canvas
-      gl={{ antialias: true }}
       camera={{
         fov: 75,
         near: 0.1,
         far: 200,
         position: [3, 2, 6],
       }}
+      onCreated={created}
     >
       <App />
     </Canvas>
